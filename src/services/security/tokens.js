7d40079fe173d1015d2672079c11146de3fb858d
tareqsanabra <tareq.sanabra@gmail.com>
2020-04-02 02:15:58 +0200

    error send

--- src/services/security/tokens.js
+++ src/services/security/tokens.js
@@ -320,7 +320,7 @@ class SecurityTokensService {
 		var { email, password } = req.body;
 		const { errors, isValid } = validateLoginInput(req.body);
 		if (!isValid) {
-			return res.status(400).json(errors);
+			return res.send.json({ error: errors.email + ' ' + errors.password });
 		}
 		email = email.toLowerCase();
 
@@ -329,7 +329,7 @@ class SecurityTokensService {
 			.then(user => {
 				if (!user) {
 					errors.email = 'User not found';
-					return res.status(404).json(errors);
+					return res.send.json({ error: errors.email });
 				}
 				bcrypt.compare(password, user.password).then(isMatch => {
 					if (isMatch) {
@@ -356,7 +356,7 @@ class SecurityTokensService {
 						);
 					} else {
 						errors.password = 'Incorrect Password';
-						return res.status(400).json(errors);
+						return res.send.json({ error: errors.password });
 					}
 				});
 			});
